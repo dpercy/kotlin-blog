b970fa2efbceec2a75ac37433d1725570e2a1219
David Percy <david.percy@mongodb.com>
2016-09-08 19:09:26 -0400

    Hello! and notes

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,4 +1,7 @@
+import spark.Spark.get
 
 fun main(args: Array<String>) {
-    println("hi")
+    // TODO get IntelliJ to work!!
+    println("Spark defaults to http://localhost:4567/") // TODO fix Spark logging
+    get("/", { req, res -> "Hello!" })
 }
