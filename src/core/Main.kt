42f7837d7720c52fd46da215de0dd6794daa3277
David Percy <david.percy@mongodb.com>
2016-09-08 18:34:04 -0400

    use main function instead of class

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,8 +1,4 @@
 
-class MainKT {
-    companion object {
-        @JvmStatic public fun main(args: Array<String>) {
-            println("hi")
-        }
-    }
+fun main(args: Array<String>) {
+    println("hi")
 }
