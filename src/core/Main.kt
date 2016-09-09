9edd04bd023d825bfb557d1ef19660d6ac17e0df
David Percy <david.percy@mongodb.com>
2016-09-09 14:45:36 -0400

    Get IntelliJ completion working

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,7 +1,7 @@
 import spark.Spark.get
+import spark.Spark
 
 fun main(args: Array<String>) {
-    // TODO get IntelliJ to work!!
     println("Spark defaults to http://localhost:4567/") // TODO fix Spark logging
     get("/", { req, res -> "Hello!" })
 }
