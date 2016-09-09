939aace7b8d08dcf9f89bd18962aaebc3e5ccd79
David Percy <david.percy@mongodb.com>
2016-09-09 18:22:34 -0400

    Render posts from mongodb

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,3 +1,5 @@
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
 import spark.*
 import spark.Spark.*
 import spark.debug.DebugScreen.enableDebugScreen
@@ -8,10 +10,10 @@ import spark.template.mustache.MustacheTemplateEngine
 DONE
 - get stack trace for 500
 - render a template
-
-TODO
 - connect to mongodb
 - render data from mongodb
+
+TODO
 - create a form
 - insert data into mongodb
 - reverse a URL
@@ -21,6 +23,7 @@ TODO
 
  */
 
+var mongoClient: MongoClient? = null
 
 fun index(request: Request?, response: Response?): ModelAndView {
     val context = hashMapOf(
@@ -30,11 +33,31 @@ fun index(request: Request?, response: Response?): ModelAndView {
     return ModelAndView(context, "resources/templates/index.html")
 }
 
+fun greet(request: Request?, response: Response?): ModelAndView {
+    val name = request!!.params("name")
+    val context = hashMapOf(
+        "name" to name
+    )
+    return ModelAndView(context, "resources/templates/greet.html")
+}
+
+fun posts(request: Request?, response: Response?): ModelAndView {
+    val postsCollection = mongoClient!!.getDatabase("test").getCollection("posts")
+
+    val context: Map<String, Any> = hashMapOf(
+        "posts" to postsCollection.find().toList()
+    )
+    return ModelAndView(context, "resources/templates/posts.html")
+}
+
 
 fun main(args: Array<String>) {
     val mte = MustacheTemplateEngine()
+    mongoClient = MongoClient(MongoClientURI("mongodb://localhost:27017"))
 
     get("/", ::index, mte)
+    get("/greet/:name", ::greet, mte)
+    get("/posts", ::posts, mte)
 
     enableDebugScreen()
 }
