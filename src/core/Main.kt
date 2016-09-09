edd24027139a6c99ff4c387b6e8fbe923a0d3836
David Percy <david.percy@mongodb.com>
2016-09-09 17:58:18 -0400

    templates

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,7 +1,40 @@
-import spark.Spark.get
-import spark.Spark
+import spark.*
+import spark.Spark.*
+import spark.debug.DebugScreen.enableDebugScreen
+import spark.template.mustache.MustacheTemplateEngine
+
+/*
+
+DONE
+- get stack trace for 500
+- render a template
+
+TODO
+- connect to mongodb
+- render data from mongodb
+- create a form
+- insert data into mongodb
+- reverse a URL
+
+- fix template resolution??
+  - avoid specifying src/templates/ before each one
+
+ */
+
+
+fun index(request: Request?, response: Response?): ModelAndView {
+    val context = hashMapOf(
+        "title" to "blog",
+        "body" to "blah blah blah"
+    )
+    return ModelAndView(context, "resources/templates/index.html")
+}
+
 
 fun main(args: Array<String>) {
-    println("Spark defaults to http://localhost:4567/") // TODO fix Spark logging
-    get("/", { req, res -> "Hello!" })
+    val mte = MustacheTemplateEngine()
+
+    get("/", ::index, mte)
+
+    enableDebugScreen()
 }
